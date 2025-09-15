17eb965719bc286f40d04402ac4a81be18f448f0
Nawel <benbachirnawel@gmail.com>
2025-09-15 15:06:02 +0200

    modif classe Titulaire

--- exercie_poo/Compte.cs
+++ exercie_poo/Compte.cs
@@ -11,12 +11,24 @@
             //Setter
             set { _solde = value; }
         }
-    private Titulaire? _titulaire;
-    public Titulaire? Titulaire {
+    private Titulaire _titulaire;
+    public Titulaire Titulaire {
         get { return _titulaire; }
         set { _titulaire = value; }
     }
+    //Constructeur paramétré
+    public Compte(string nom, string prenom, string adresse, string codepostal, string ville, string tel, string email, decimal solde)
+    {
+        _solde = solde;
+        _titulaire = new Titulaire(nom, prenom, adresse, codepostal, ville, tel, email);
+    }
 
+    //constructeur par défaut
+    public Compte()
+    {
+        _solde = 0;
+        _titulaire = new Titulaire("", "", "", "", "", "", "");
+    }
     public void Afficher()
     {
         Console.WriteLine($"Solde:{this.Solde}");
