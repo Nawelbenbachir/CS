ecb1e17c98974dad281e40617c4456fe078122ea
Nawel <benbachirnawel@gmail.com>
2025-09-15 12:11:06 +0200

    ajout solde

--- exercie_poo/Compte.cs
+++ exercie_poo/Compte.cs
@@ -2,6 +2,11 @@ namespace ExerciceCompte
 {
     class Compte
     {
-        
+         private float _solde=0;
+        public float Solde
+    {
+        get { return _solde; }
+        set { _solde = value; }
+        }
     }
 }
