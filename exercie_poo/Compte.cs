d079739e3b5f890b518802e0ed91b67f58528c7b
Nawel <benbachirnawel@gmail.com>
2025-09-15 12:26:46 +0200

    ajout Afficher()

--- exercie_poo/Compte.cs
+++ exercie_poo/Compte.cs
@@ -2,11 +2,19 @@ namespace ExerciceCompte
 {
     class Compte
     {
-         private float _solde=0;
-        public float Solde
-    {
-        get { return _solde; }
-        set { _solde = value; }
+        //Variable interne pour stocker le solde
+        private decimal _solde;
+        //Propriété publique pour manipuler le solde
+        public decimal Solde
+        {
+            //Getter
+            get { return _solde; }
+            //Setter
+            set { _solde = value; }
+        }
+        public void Afficher()
+        {
+            Console.WriteLine($"Solde:{this.Solde}");
         }
     }
 }
