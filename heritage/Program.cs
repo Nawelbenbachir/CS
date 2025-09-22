e0afc17e42a8c29853e8a56f4053f26cab52afcf
Nawel <benbachirnawel@gmail.com>
2025-09-22 11:35:10 +0200

    mis a jour heritage

--- heritage/Program.cs
+++ heritage/Program.cs
@@ -3,11 +3,29 @@
     public static void Main(string[] args)
     {
         //instanciation d'un Utilisateur
-        Utilisateur Util1 = new Utilisateur();
-        Util1.Nom = "Benbachir";
-        Util1.Prenom = "Nawel";
+        // Utilisateur Util1 = new Utilisateur();
+        // Util1.Nom = "Benbachir";
+        // Util1.Prenom = "Nawel";
+        // Util1.Afficher();
+
+        Utilisateur Util1 = new Utilisateur("Nawel", "Benbachir");
         Util1.Afficher();
+
+        //client est une instance (un objet) de la classe Client
+        // Client Client1 = new Client();
+        // Client1.Nom = "Dupont";
+        // Client1.Prenom = "Jacques";
+        // Client1.Adresse = "Place Jean Jaurès 84740 Velleron";
+        // Client1.Afficher();
+
+        Client Client1 = new Client("Jacques", "Dupont", "Place Jean Jaurès 84740 Velleron");
+        Client1.Afficher();
+
+
+        Employe Employe1 = new Employe();
+        Employe1.Nom = "Durand";
+        Employe1.Prenom = "Jerome";
+        Employe1.Poste = "Secrétaire";
+        Employe1.Afficher();
     }
 }
-
-
--- heritage/Utilisateur.cs
+++ heritage/Utilisateur.cs
@@ -2,11 +2,65 @@ public class Utilisateur
 {
     public string Nom { get; set; }
     public string Prenom { get; set; }
-
+    //Constructeur par défaut de la classe Utilisateur
+    //permet d'initialiser les propriétés de l'objet pour qu'elles ne soient pas nulles
+    //même nom que la classe, pas de type de retour
+    public Utilisateur()
+    {
+        Nom = "";
+        Prenom = "";
+    }
+    public Utilisateur(string prenom, string nom)
+    {
+        Nom = nom;
+        Prenom = prenom;
+    }
     public void Afficher()
     {
-    Console.WriteLine($" {Nom}, {Prenom}");
+        Console.WriteLine($"Utilisateur : {Nom} {Prenom}");
 
     }
 
+}
+//Classe hérité Administrateur 
+public class Administrateur : Utilisateur
+{
+}
+
+//Classe héritée Client
+public class Client : Utilisateur
+{
+    public string Adresse { get; set; }
+    public Client()
+    {
+        Adresse = "";
+    }
+    public Client(string prenom, string nom, string adresse): base(prenom,nom)
+    {
+        //inutile grâce à :base qui permet d'appeler le constructeur de la classe parent 
+        //base() pour appeler le constructeur vide (par défaut si non précisé)
+        Prenom = prenom;
+        Nom = nom;
+        Adresse =adresse;
+    }
+    //Surcharge de la méthode Afficher qui devient alors prioritaire à celle de la classe Utilisateur
+    //On ajoute new pour indiquer que c'est cette nouvelle méthode qu'il faut utiliser
+    public new void Afficher()
+    {
+        Console.WriteLine($"Client : {Nom} {Prenom} {Adresse}");
+    }
+}
+
+public class Employe : Utilisateur
+{
+    public string Poste { get; set; }
+    public Employe()
+    {
+        Poste = "";
+    }
+
+     public new void Afficher()
+    {
+        Console.WriteLine($"Employé : {Nom} {Prenom} {Poste}");
+    }
 }
